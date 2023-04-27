813ed58f0f7c7375eb62ab1e17714aa5923ef8f1
DmytroSadovskyi <111589023+DmytroSadovskyi@users.noreply.github.com>
2023-04-27 14:34:07 +0000

    Deploying to gh-pages from @ DmytroSadovskyi/goit-react-hw-05-movies@3d703e6e3e670d546dd71847042b5997f8ae6f87 🚀

---
+++ static/js/681.26006936.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[681],{9681:function(e,i,l){l.r(i);var r=l(184);i.default=function(){return(0,r.jsxs)("ul",{children:[(0,r.jsx)("li",{children:"Review 1"}),(0,r.jsx)("li",{children:"Review 2"}),(0,r.jsx)("li",{children:"Review 3"}),(0,r.jsx)("li",{children:"Review 4"}),(0,r.jsx)("li",{children:"Review 5"})]})}}}]);
+//# sourceMappingURL=681.26006936.chunk.js.map
