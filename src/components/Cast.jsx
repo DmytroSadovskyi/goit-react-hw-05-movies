9355bc4d52084d0979a964c9c70cf2639a11d767
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-27 23:03:41 +0300

    Update Cast.jsx

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -31,38 +31,32 @@ const Cast = () => {
       ) : (
         <>
           {isLoading && <Loader />}
-          <ul
-            style={{
-              display: 'flex',
-              flexWrap: 'wrap',
-              gap: '10px',
-              overflowY: 'scroll',
-              width: '800px',
-              height: '400px',
-              listStyle: 'none',
-            }}
-          >
-            {movieCast.map(({ id, character, name, profile_path }) => (
-              <li key={id}>
-                <img
-                  src={
-                    profile_path
-                      ? `https://image.tmdb.org/t/p/w300${profile_path}`
-                      : 'https://dummyimage.com/500x400/ff6b08/fff.jpg&text=Opps,+no+image...'
-                  }
-                  alt={name}
-                  width="150"
-                  height="200"
-                />
-                <p>
-                  <span>{name}</span>
-                </p>
-                <p>
-                  <span>{character}</span>
-                </p>
-              </li>
-            ))}
-          </ul>
+          {movieCast.length === 0 && !error ? (
+            <h2>We didn't find info about cast</h2>
+          ) : (
+            <ul>
+              {movieCast.map(({ id, character, name, profile_path }) => (
+                <li key={id}>
+                  <img
+                    src={
+                      profile_path
+                        ? `https://image.tmdb.org/t/p/w300${profile_path}`
+                        : 'https://dummyimage.com/500x400/ff6b08/fff.jpg&text=Opps,+no+image...'
+                    }
+                    alt={name}
+                    width="150"
+                    height="200"
+                  />
+                  <p>
+                    <span>{name}</span>
+                  </p>
+                  <p>
+                    <span>{character}</span>
+                  </p>
+                </li>
+              ))}
+            </ul>
+          )}
         </>
       )}
     </>
