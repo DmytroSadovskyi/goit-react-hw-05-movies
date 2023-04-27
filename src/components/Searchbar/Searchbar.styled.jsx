1ffcc10cb36f78e72689f2d6ee4fb71f773a1d40
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-27 21:31:06 +0300

    Create Searchbar.styled.jsx

---
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled';
+
+export const SearchForm = styled.form`
+  display: flex;
+  margin: 0 auto;
+  align-items: center;
+
+  width: 300px;
+  border-radius: 3px;
+  overflow: hidden;
+  font-size: 20px;
+`;
+
+export const SearchInput = styled.input`
+  display: inline-block;
+  width: 100%;
+  height: 48px;
+  padding-left: 20px;
+  font-size: 20px;
+  border: none;
+
+  ::placeholder {
+    font-size: 18px;
+  }
+`;
+
+export const SearchButton = styled.button`
+  display: inline-block;
+  padding: 5px;
+  width: 48px;
+  height: 48px;
+  border: 0;
+  background-color: white;
+  cursor: pointer;
+
+  svg {
+    fill: black;
+    font-size: 24px;
+  }
+`;
+
+export const SearchButtonLabel = styled.span`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  clip-path: inset(50%);
+  border: 0;
+`;
