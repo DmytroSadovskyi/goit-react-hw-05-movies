2817fe0b8c9e5edb7393c69fbccbf8a55b3a8e3e
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-27 23:03:57 +0300

    Update Reviews.jsx

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,12 +1,54 @@
+import { useState, useEffect } from 'react';
+import { fetchMovieReviews } from 'services/movie-api';
+import { useParams } from 'react-router-dom';
+import Loader from './Loader/Loader';
+
 const Reviews = () => {
+  const { Id } = useParams();
+  const [movieReviews, setMovieReviews] = useState([]);
+  const [totalResults, setTotalResults] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadMovieReviews = async () => {
+      try {
+        setIsLoading(true);
+        const { data, results, total_results } = await fetchMovieReviews(Id);
+        setMovieReviews(results);
+        setTotalResults(total_results);
+        console.log(data);
+      } catch (error) {
+        setError('Error while loading data. Try again later.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    loadMovieReviews();
+  }, [Id]);
+
   return (
-    <ul>
-      <li>Review 1</li>
-      <li>Review 2</li>
-      <li>Review 3</li>
-      <li>Review 4</li>
-      <li>Review 5</li>
-    </ul>
+    <>
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <>
+          {isLoading && <Loader />}
+          {totalResults === 0 && !error ? (
+            <h2>We didn't find any reviews for this movie</h2>
+          ) : (
+            <ul>
+              {movieReviews.map(({ id, author, content }) => (
+                <li key={id}>
+                  <h3>{author}</h3>
+                  <p>{content}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
