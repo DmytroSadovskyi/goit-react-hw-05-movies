d88d77c06d46b719f18da76259c06145dcb92d39
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-27 23:03:44 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,17 @@
 import { BallTriangle } from 'react-loader-spinner';
-import { Background } from './Loader.styled';
+import { LoaderContainer } from './Loader.styled';
 
 const Loader = () => {
   return (
-    <Background>
+    <LoaderContainer>
       <BallTriangle
         height={100}
         width={100}
         radius={5}
-        color="#4fa94d"
+        color="blue"
         ariaLabel="ball-triangle-loading"
       />
-    </Background>
+    </LoaderContainer>
   );
 };
 
