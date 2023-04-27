af223de9d386a6237f9de9eab538f54bb2ce3260
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-27 23:04:04 +0300

    Update SharedLayout.styled.jsx

--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -8,7 +8,8 @@ export const Header = styled.header`
   gap: 12px;
   padding: 8px 0;
   margin-bottom: 16px;
-  border-bottom: 1px solid black;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 
   > nav {
     display: flex;
