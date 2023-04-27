3606ed8c0be5efd0d520d7eb39f34a7648a5d9f8
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-27 21:08:31 +0300

    Create SharedLayout.styled.jsx

---
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  gap: 12px;
+  padding: 8px 0;
+  margin-bottom: 16px;
+  border-bottom: 1px solid black;
+
+  > nav {
+    display: flex;
+  }
+`;
+
+export const Logo = styled.p`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+  margin: 0;
+  gap: 5px;
+`;
+
+export const Link = styled(NavLink)`
+  padding: 8px 16px;
+  border-radius: 4px;
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+
+  &.active {
+    color: white;
+    background-color: blue;
+  }
+`;
