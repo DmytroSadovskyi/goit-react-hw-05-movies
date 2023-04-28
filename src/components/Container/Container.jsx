608c2d169294ac009223b35942499868858f9c4a
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-04-28 23:45:09 +0300

    Update Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,3 +1,9 @@
 import { Wrapper } from './Container.styled';
+import PropTypes from 'prop-types';
+const Container = ({ children }) => <Wrapper>{children}</Wrapper>;
 
-export const Container = ({ children }) => <Wrapper>{children}</Wrapper>;
+Container.propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+export default Container;
